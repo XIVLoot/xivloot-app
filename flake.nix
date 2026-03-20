213ba4a140ae541aa6dca6e513869ff58466e4ff
Ricky Chittaphone <rickychittaphone@gmail.com>
2026-03-20 11:29:11 -0400

    feat: Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "XIVLoot Angular Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { 
+          inherit system; 
+          config.allowUnfree = true; 
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # NodeJS and angular packages
+            nodejs_20
+            
+            # Utilities
+            git
+          ];
+
+          shellHook = ''
+            echo "-------------------------------------------------------"
+            echo "🚀 XIVLoot Angular Dev Environment"
+            echo "-------------------------------------------------------"
+            echo
+
+            echo "Installing the depedencies..."
+            npm install -g @angular/cli
+            clear
+            npm install
+            ng serve -o
+          '';
+        };
+      });
+}
